5a98c3ab59318698f36fe20e384d1775bcbb50b9
Allan Vítor <awallanvitor@gmail.com>
2025-01-22 17:18:03 -0300

    Segunda aula, funções e operadores

--- fundamentos/funcoes.js
+++ fundamentos/funcoes.js
@@ -8,3 +8,48 @@
  }
 
  hello()
+
+ //função atribuida
+ const hello2 = function () {
+   console.log("Hello function assigned")
+ }
+
+ console.log(typeof(hello2))
+ hello2()
+ 
+ // Arrow function (simplificação da função atribuída)
+ // function ---- =>
+ const hello3 = () => {
+   console.log("Hello arrow function")
+
+ } 
+
+ console.log(typeof(hello3))
+ hello3()
+
+ //Função com passagens e parâmetros e retorno
+ // Funções simples
+ function somarS(num1, num2) {
+   return (console.log(num1 + num2))
+
+ }
+
+somarS (1, 0)
+
+//Função atribuída
+const somarA = function(num1, num2){
+      return(console.log(num1 + num2))
+}
+
+somarA(1, 0)
+
+//Arrow Function
+const somarAF = function(num1, num2){
+   return(console.log(num1 + num2))
+}
+
+somarA(0, 0)
+
+//Arrow Function simplificada (o retorno é implícito)
+const  somarAFS = (num1,num2) => console.log (num1 + num2)
+somarAFS (3, 0)
---
+++ fundamentos/operadores.js
@@ -0,0 +1,32 @@
+/**
+ * Aritméticos
+ * Aritméticos de atribuição
+ * Comparação
+ * Logicos
+
+**/
+
+console.clear()
+console.log("Operadores aritméticos")
+console.log("+ | soma")
+console.log("- | subtração")
+console.log("* | multiplicação")
+console.log("/ | divisão")
+console.log("%| módulo (resto da divisão)")
+console.log("")
+console.clear()
+console.log("Operadores aritméticos de atribuição")
+console.log("+ | soma e atribuição de variável")
+console.log("- | subtração e atribuição de variável")
+console.log("* | multiplicação e atribuição de variável")
+console.log("/ | divisão e atribuição de variável")
+console.log("++ | somar 1 ao resultado")
+console.log("-- | subtrair 1 do resultado")
+console.log ("")
+console.log("Comparadores")
+console.log("> | maior que")
+console.log("=== | idêntico a")
+console.log("!== | não idêntico a")
+console.log ("&& | And")
+console.log ("|| | OR")
+console.log ("! | NOT")
